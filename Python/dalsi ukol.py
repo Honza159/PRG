d9e4477f90381aab9c7be381c2b7e25c610b9f04
jan.peske <peskejan@gmail.com>
2026-02-20 12:30:43 +0100

    wedg

--- Python/dalsi ukol.py
+++ Python/dalsi ukol.py
@@ -1,5 +1,14 @@
+radky = 0
+slova = 0
+znaky = 0
 with open("praha.txt", "r", encoding="utf-8") as f:
-    text = f.read().lower()
+    text = f.read()
+    text_lower = text.lower()
+    pocet = text_lower.count("praha")
 
-pocet = text.count("praha")
-print(f"{pocet}x slova praha")
+    radky = text.count("\n") + 1
+    slova = len(text.split())
+    znaky = len(text)
+
+print(f"{pocet}x slovo praha.")
+print("řádky:", radky, "slova:", slova, "znaky:", znaky)
